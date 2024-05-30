9dbcbf8f949a277a47ab413fe5a939b5f694d4ed
Awe <liyawei@hao3you.com>
2024-05-30 11:28:44 +0800

    navbar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,34 +1,19 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
+import {viteBundler} from '@vuepress/bundler-vite'
+import {defaultTheme} from '@vuepress/theme-default'
+import {defineUserConfig} from 'vuepress'
+import navConf from "./config/navConf";
 
 export default defineUserConfig({
-  bundler: viteBundler(),
+    bundler: viteBundler(),
 
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+    lang: 'zh-CN',
+    title: 'Awe-docs',
+    description: '这是我的第一个 VuePress 站点',
+    head: [['link', {rel: 'icon', href: '/logo.png'}]],
 
-  theme: defaultTheme({
+    theme: defaultTheme({
         docsDir: 'docs',
         // 默认主题配置
-        navbar: [
-            {
-                text: '🏠 首页',
-                link: '/',
-            },
-            {
-                text: '📖 我的网站',
-                link: 'https://www.aweew.com',
-            },
-            {
-                text: '🌏 Github',
-                link: 'https://github.com/aweew',
-            },
-            {
-                text: '📙 GitEE',
-                link: 'https://gitee.com/aweew',
-            },
-        ],
+        navbar: navConf,
     }),
 })
---
+++ docs/.vuepress/config/navConf.ts
@@ -0,0 +1,24 @@
+export default [
+    {text: '🏠 首页', link: '/'},
+    {text: '💻 后端', children: [
+            {text: 'Java', link: '/backend/java/'},
+            {text: 'Spring', link: '/backend/spring/'},
+            {text: 'Nginx', link: '/backend/nginx/'},
+            {text: 'MySQL', link: '/backend/mysql/'},
+            {text: 'Redis', link: '/backend/redis/'},
+            {text: 'Docker', link: '/backend/docker/'},
+    ]},
+    {text: '📗 前端', children: [
+            {text: 'Web', link: '/frontend/web/'},
+            {text: 'JavaScript', link: '/frontend/javascript/'},
+            {text: 'TypeScript', link: '/frontend/typescript/'},
+            {text: 'Vue', link: '/frontend/vue/'},
+            {text: 'React', link: '/frontend/React/'},
+    ]},
+    {text: '🎯 算法', link: '/algorithms/'},
+    {text: '🥤 更多', children: [
+            {text: '博客', link: 'https://www.aweew.com'},
+            {text: 'Github', link: 'https://github.com/aweew'},
+            {text: 'Gitee', link: 'https://gitee.com/aweew'},
+    ]},
+];
